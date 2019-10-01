21d6afdab9b85fa0c8ea45aa160c469d50e381a2
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-10-01 22:04:50 +0530

    .map() function

--- JavaScript/Basics/app.js
+++ JavaScript/Basics/app.js
@@ -1,6 +1,6 @@
 // hello world
-function hello(){
-    console.log('hello world');
+function hello() {
+  console.log("hello world");
 }
 
 hello();
@@ -15,9 +15,9 @@ if(true){
 console.log(a);*/
 
 let a = 25;
-if(true){
-    let a = 35;
-    console.log(a);
+if (true) {
+  let a = 35;
+  console.log(a);
 }
 console.log(a);
 
@@ -26,43 +26,55 @@ console.log(a);
 // Hoisting is if we have declared the function after calling it, then the declaration will move up
 // close to where function was called first.
 
-
 //FUNCTIONS
 
-function abc(a, b, c){
-    console.log(a + " " + b + " " + c);
+function abc(a, b, c) {
+  console.log(a + " " + b + " " + c);
 }
 abc(10, 20, 30);
 
-function func(a, b, c = 30){
-    console.log(a + " " + b + " " + c);
+function func(a, b, c = 30) {
+  console.log(a + " " + b + " " + c);
 }
-func(10, 20);
+func(10, 20); // This will give error as c is not defined.
 
 /*function fun(a, b){
     console.log(a + " " + b + " " + c);
 }
-fun(10, 20, 30);*/ // This will give error as c is not defined.
-
-function rest(...a){
-    console.log(a);
-    console.log(a.length);
+fun(10, 20, 30);*/ function rest(
+  ...a
+) {
+  console.log(a);
+  console.log(a.length);
 }
 rest(10, 20, 30);
 // Rest operator it will merge various elements into an array
 
-var hi = function hello(){
-    console.log('hello');
-}
+var hi = function hello() {
+  console.log("hello");
+};
 hi();
 // Anonymous function
 
-var kiddan = (a=30, b=50) => {
-    console.log(a+b);
-}
+var kiddan = (a = 30, b = 50) => {
+  console.log(a + b);
+};
 kiddan(); //Arrow functions
 
 var ohwow = value => {
-    console.log(value/2);
-}
+  console.log(value / 2);
+};
 ohwow(70); // Arrow function with single argument
+
+
+// .map() 
+var officers = [
+  { id: 20, name: "Captain Piett" },
+  { id: 24, name: "General Veers" },
+  { id: 56, name: "Admiral Ozzel" },
+  { id: 88, name: "Commander Jerjerrod" }
+];
+
+const officersIds = officers.map(officer => officer.id);
+
+console.log(officersIds);
