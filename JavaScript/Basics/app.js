dca8977712e3a0022121e0fa271866e625e9bf03
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-10-01 09:53:05 +0530

    hello world and hoisting

---
+++ JavaScript/Basics/app.js
@@ -0,0 +1,28 @@
+// hello world
+function hello(){
+    console.log('hello world');
+}
+
+hello();
+
+//hoisting of variables
+
+/*var a = 25;
+if(true){
+    var a = 35;
+    console.log(a);
+}
+console.log(a);*/
+
+let a = 25;
+if(true){
+    let a = 35;
+    console.log(a);
+}
+console.log(a);
+
+// So 'let' has local scope while 'var' has global scope
+
+// Hoisting is if we have declared the function after calling it, then the declaration will move up
+// close to where function wa called first.
+
