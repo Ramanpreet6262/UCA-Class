4823915f5c3910960e9f4dfdf0d79b9b0fd28ba7
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-10-09 11:49:23 +0530

    promises and named func..

--- JavaScript/ES6/index.js
+++ JavaScript/ES6/index.js
@@ -55,3 +55,66 @@ console.log('After');
 function func1(callback){
     setTimeout(() => {callback('hello kiddan')},2000);
 }
+
+// Callbacks
+// concept eh hai ki ikk di output(andar walle di) bahar walle lai input hai .... okok .... 
+
+// Definitions of func2, func3, func4
+function func2(callback){
+    setTimeout(() => {callback({id: 1, name: 'abc'})},2000);
+};
+
+function func3(x, callback){
+    setTimeout(() => {
+        console.log(x);
+        callback([4,5,6]);
+    },2000);
+}
+
+function func4(y, callback){
+    setTimeout(() => {
+        console.log(y);
+        callback(6);
+    },2000);
+}
+
+// Calling of callback functions
+// func2((x) => {
+//     func3(x.id, (y) => {
+//         func4(y[0], (z) => {
+//             console.log(z);
+//         });
+//     });
+// });
+
+
+// 2. Named Function
+// By this we will reduce callback functions i.e. function inside function and so on.....
+
+// New calling with named functions
+
+func2(dfunc3);
+
+function dfunc3(x){
+    func3(x.id, dfunc4);
+}
+
+function dfunc4(y){
+    func4(y[0], display);
+}
+
+function display(z){
+    console.log(z);
+}
+
+
+//3. Promises
+// Promise is a new class and it has 2 parts resolve which is handled by .then and reject which is handled by .catch
+
+let p = new Promise((resolve, reject) => {
+    resolve('hello promise');
+})
+
+p.then((x) => {console.log(x)});
+
+// .then matlab if this promise gets resolved then .then will be executed
