8b071930673f7c4eeefa4ca7c3ef1eced3b8add1
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-10-09 10:58:01 +0530

    import export and callbacks

---
+++ JavaScript/ES6/external.js
@@ -0,0 +1,38 @@
+// let a = 10;
+
+// let b = () => {
+//     return 20;
+// };
+
+// let c = {
+//     name: "hello",
+//     section: "uca"
+// }
+
+// export{a, b, c};
+
+//Exporting at end all of them together
+
+// -----------------------------------------------------------
+
+// Exporting them independently
+// export let a = 10;
+
+// export let b = () => {
+//     return 20;
+// };
+
+// export let c = {
+//     name: "hello",
+//     section: "uca"
+// }
+
+// ------------------------------------------
+
+//Default export
+
+let a = 10;
+export default a;
+
+// Keep in mind we can only export one variable as default not more only one......
+
--- JavaScript/ES6/index.js
+++ JavaScript/ES6/index.js
@@ -1,1 +1,57 @@
-console.log("kiddan");
+// Importing them with same name as in external file
+// import {a, b, c} from './external';
+
+// console.log(a + " " + b() + " " + c.name);
+
+// ----------------------------------------------------------
+
+// Importing with different names
+// import {a as x, b as y, c as z} from './external';
+
+// console.log(x + " " + y() + " " + z.name);
+
+// -------------------------------------------------------------
+
+// Default export working....
+// As default exports works for 1 variable, so we will import one variable
+
+import a from './external';
+console.log(a);
+
+
+
+
+
+
+
+// Promises
+
+// 4 methods
+
+/*
+1. Callback
+2. Named func
+3. Promises
+4. Async Await
+*/
+
+// This is asynchronous code....
+console.log("Before");
+console.log(func());
+console.log('After');
+
+function func(){
+    setTimeout(() => {},2000);
+}
+
+// first undefined will be printed and later on 'hi' when set timeout finishes.....
+
+
+// 1. Callback
+console.log("Before");
+console.log(func1((x)=> {console.log(x)}));
+console.log('After');
+
+function func1(callback){
+    setTimeout(() => {callback('hello kiddan')},2000);
+}
