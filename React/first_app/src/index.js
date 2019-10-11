0310b1f671a058a5389fa62faf0eaee53c9b3189
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-10-11 11:05:36 +0530

    createElement, JSX and Components

--- React/first_app/src/index.js
+++ React/first_app/src/index.js
@@ -1,9 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
- 
-const element = React.createElement('ul', null,
-    React.createElement('li', null, 'Todo1'),
-    React.createElement('li', null, 'Todo2')
-);
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+// const element = React.createElement('h1', null, 'hello world!!!');
+// hello world code
+
+// const element = React.createElement('ul', null,
+//     React.createElement('li', null, 'Todo1'),
+//     React.createElement('li', null, 'Todo2')
+// );
 // But in this way it becomes quite tedious to do React.createElement() again and again
-ReactDOM.render(element, document.getElementById('root'));
+
+// So we made an array
+// const tasks = ['todo1', 'todo2', 'todo3'];
+
+// const element = <ul>
+// {tasks.map((task, index)=>{return <li key={index}>{task}</li>})}
+// </ul>
+// const element = React.createElement('ul', null,
+//     tasks.map((task, index) => {
+//     return React.createElement('li', null, task);
+// }));
+
+// But it also became tedious this way so we started using JSX....
+
+//JSX
+
+// const element = <ul>
+//   <li>{tasks[0]}</li>
+//   <li>{tasks[1]}</li>
+//   <li>{tasks[2]}</li>
+// </ul>
+// This is using JSX
+
+// const element = <ol>
+// {tasks.map((task, index)=>{return <li value={index}>{task}</li>})}
+// </ol>
+// This is same as above with only change i.e. taking data from array
+
+
+
+
+
+
+// Using Components
+
+// Remember component class name should always start with a capital letter
+class List extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Todo1</li>
+        <li>Todo1</li>
+        <li>Todo3</li>
+        <li>Todo4</li>
+      </ul>
+    );
+  }
+}
+
+class Title extends Component {
+  render() {
+    return "Hello";
+  }
+}
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Title />
+        <List />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main/>, document.getElementById("root"));
