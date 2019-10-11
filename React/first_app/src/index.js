d6851430694deb42a216374082cc6e6a1fcff845
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-10-11 10:18:38 +0530

    list using createElement

--- React/first_app/src/index.js
+++ React/first_app/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const element = React.createElement('h1', null, 'hello world!!!');
+ 
+const element = React.createElement('ul', null,
+    React.createElement('li', null, 'Todo1'),
+    React.createElement('li', null, 'Todo2')
+);
+// But in this way it becomes quite tedious to do React.createElement() again and again
 ReactDOM.render(element, document.getElementById('root'));
