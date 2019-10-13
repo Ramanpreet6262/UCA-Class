82cd8a29085a9f1353b959ac34e45a3bb9c69a12
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-10-13 15:16:38 +0530

    basic get route

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res, next) => {
+    console.log('HI');
+    res.send('<h1>kiddan</h1>')
+})
+
 const port = 5000 || process.env.PORT; 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
