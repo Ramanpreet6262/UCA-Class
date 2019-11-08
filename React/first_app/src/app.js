91acd48180c98b4057f4c836dc091cc8f892c8cb
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-11-09 04:53:32 +0530

    added state

--- React/first_app/src/app.js
+++ React/first_app/src/app.js
@@ -1,36 +1,39 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Person from './person';
-
-let person = [
-    {
-        name: 'Raghav',
-        id: 1
-    },
-    {
-        name: 'Raman',
-        id:  2
-    },
-    {
-        name: 'Rishabh',
-        id: 3
-    }
-];
+import Person from "./person";
 
 class App extends Component {
-    render(){
-        return(
-            // To return only name and id of User at index 0 in a single line..
-            // <Person name = {person[0].name} and id = {person[0].id} /> 
+  constructor() {
+    super();
+    this.state = {
+      person: [
+        {
+          name: "Raghav",
+          id: 1
+        },
+        {
+          name: "Raman",
+          id: 2
+        },
+        {
+          name: "Rishabh",
+          id: 3
+        }
+      ]
+    };
+  }
 
-            // To return all users that are in array of objects defined above, use Map..
-            person.map((user, index) => {
-                return(
-                    <Person key={index} name={user.name} id={user.id} />
-                );
-            })
-        );
-    }
+  render() {
+    return (
+      // To return only name and id of User at index 0 in a single line..
+      // <Person name = {person[0].name} and id = {person[0].id} />
+
+      // To return all users that are in array of objects defined above, use Map..
+      this.state.person.map((user, index) => {
+        return <Person key={index} name={user.name} id={user.id} />;
+      })
+    );
+  }
 }
 
 export default App;
