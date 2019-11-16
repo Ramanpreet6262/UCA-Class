b495b5b894e202dc9cd3a938d154ee3e520785c8
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-11-16 10:44:15 +0530

    added booklist and Home page route

--- NodeJS_Library_Portal/index.js
+++ NodeJS_Library_Portal/index.js
@@ -5,6 +5,22 @@ const app = express();
 
 dotenv.config();
 
+var books = [
+    {
+        "id" : 1,
+        "bookName" : "The Alchemist"
+    }
+]
+
+app.get("/", (req, res) => {
+    // console.log(req);
+    res.send('Library Portal');
+});
+
+app.get("/booklist", (req, res) => {
+    res.send(books);
+});
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
